82647f807c6e098cfe68ba5b7dd1021fedb57cab
arturoless <43190646+arturoless@users.noreply.github.com>
2019-03-15 09:22:13 -0600

    color

---
+++ src/components/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-app>
+      <!-- <v-toolbar app color="primary">
+      <v-toolbar-title class="headline text-uppercase">
+        <span class="title">planty</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar> -->
+    <div class="headline text-xs-center pa-5">
+    </div>
+    <v-bottom-nav
+      :active.sync="bottomNav"
+      :value="true"
+      absolute
+      color="transparent"
+    >
+      <v-btn
+        color="primary"
+        flat
+        value="recent"
+      >
+        <span>Recent</span>
+        <v-icon>history</v-icon>
+      </v-btn>
+
+      <v-btn
+        color="primary"
+        flat
+        value="favorites"
+      >
+        <span>Favorites</span>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn
+        color="primary"
+        flat
+        value="nearby"
+      >
+        <span>Nearby</span>
+        <v-icon>place</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+  </v-app>
+</template>
+<style>
+  .title{
+    color:"white"
+  }
+</style>
+<script>
+  export default {
+    data () {
+      return {
+        bottomNav: 'recent'
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+// import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#64dd17',
+    plight: '#9cff57',
+    pdark: '#1faa00',
+    secondary: '#76ff03',
+    slight: '#b0ff57',
+    sdark: '#32cb00',
+    error: '#D50000'
+  }
+})
+
 
 new Vue({
   render: h => h(App),
