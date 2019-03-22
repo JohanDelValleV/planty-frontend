f60491ea6a14aefe18067693c2d490c7c16ea591
JohanDelValleV <43177845+JohanDelValleV@users.noreply.github.com>
2019-03-22 10:09:30 -0600

    More ideas

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -15,37 +15,37 @@
             Cerrar
             </v-btn>
         </v-snackbar>
-            <v-container style="position: relative;" class="text-xs-center">
-                <v-card max-height="auto" max-width="auto" class="borde">
+        <div class="container">
+            <v-flex sm4 offset-sm4>
+                <v-card class="card">
                     <v-layout>
                         <div class="container">
-                            <v-card-title>
-                                <div>
-                                    <v-icon color="primary" size="65px">local_florist</v-icon>
-                                    <span class="titulo"><strong>planty</strong></span>
-                                </div>
-                            </v-card-title>
-                            <v-card-text>
-                            <v-form>
-                                <v-text-field outline prepend-inner-icon="person" name="Username" label="Username" 
-                                v-model="username"
-                                :rules="[rules.username]"
-                                clearable primary></v-text-field>
-                                <v-text-field outline prepend-inner-icon="lock" name="Password" label="Password" type="password" 
-                                :rules="[rules.validatePassword]"
-                                v-model="password"
-                                @keyup.enter="login"
-                                autocomplete
-                                primary></v-text-field>
-                                <v-card-actions>
-                                    <v-btn large depressed block dark color="primary" class="button" v-on:click="login()">Login</v-btn>
-                                </v-card-actions>
-                            </v-form>
-                            </v-card-text>
+                            <div>
+                                <v-icon color="primary" size="65px">local_florist</v-icon>
+                                <span class="titulo"><strong>planty</strong></span>
+                            </div>
+                            <div>
+                                <v-form>
+                                    <v-text-field outline prepend-inner-icon="person" name="Username" label="Username" 
+                                    v-model="username"
+                                    :rules="[rules.username]"
+                                    clearable primary></v-text-field>
+                                    <v-text-field outline prepend-inner-icon="lock" name="Password" label="Password" type="password" 
+                                    :rules="[rules.validatePassword]"
+                                    v-model="password"
+                                    @keyup.enter="login"
+                                    autocomplete
+                                    primary></v-text-field>
+                                    <v-card-actions>
+                                        <v-btn large depressed block dark color="primary" class="button" v-on:click="login()">Login</v-btn>
+                                    </v-card-actions>
+                                </v-form>
+                            </div>
                         </div>
                     </v-layout>
                 </v-card>
-            </v-container>
+            </v-flex>
+        </div>
     </v-app>
 </template>
 
@@ -94,14 +94,19 @@ export default {
         font-size: 500%;
     }
     .container {
-        margin-top: -1%;
+        margin-top: 5%;
     }
-    .borde {
+    /* .borde {
         border-radius: 5px;
         padding-top: 10%;
         padding-bottom: 10%;
         padding-left: 25%;
         padding-right: 25%;
+    } */
+    .card {
+        border: 1px solid #dfe1e5;
+        border-radius: 8px;
+        box-shadow: none;
     }
     .button{
         border-radius: 5px;
